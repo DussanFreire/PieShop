1b4384cdfc6582514112629d6bf4d28363ace39b
DussanFreire <dussanfreire@gmail.com>
2025-03-17 10:20:30 -0400

    feat and bug: bootstrap import problem solved and contact tag helper created

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using dotnet.Models;
+﻿using dotnet.Models;
 using dotnet.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet.Controllers
 {
@@ -17,18 +13,32 @@ namespace dotnet.Controllers
         {
             _pieRepository = pieRepository;
             _categoryRepository = categoryRepository;
-
+            
         }
 
-        public IActionResult List()
+        public ViewResult List(string category)
         {
-            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "All pies");
-            return View(pieListViewModel);
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id)
         {
-            Pie? pie = _pieRepository.GetPieById(id);
+            var pie = _pieRepository.GetPieById(id);
             if (pie == null)
                 return NotFound();
 
